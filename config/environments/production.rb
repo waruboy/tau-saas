a0fba3ecc97d5a11d5853a74ec17468e836896f8
Taufiq Muhammadi <waruboy@yahoo.com>
2017-02-03 10:51:07 +0700

    prepare for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Sendgrid
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'tau-saas-pm.herokuap.com', protocol: 'https' }
 end
