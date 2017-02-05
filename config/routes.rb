ea4b9cd2d2f5e0413ad0a7cadf1877d60a30f1a4
Taufiq Muhammadi <waruboy@yahoo.com>
2017-02-05 08:13:12 +0700

    confirmation system

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
 
    root :to => "home#index"
 
-    
+
   # *MUST* come *BEFORE* devise's definitions (below)
-  as :user do   
-    match '/user/confirmation' => 'milia/confirmations#update', :via => :put, :as => :update_user_confirmation
+  as :user do
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
-  devise_for :users, :controllers => { 
+  devise_for :users, :controllers => {
     :registrations => "milia/registrations",
-    :confirmations => "milia/confirmations",
-    :sessions => "milia/sessions", 
-    :passwords => "milia/passwords", 
+    :confirmations => "confirmations",
+    :sessions => "milia/sessions",
+    :passwords => "milia/passwords",
   }
 
 
