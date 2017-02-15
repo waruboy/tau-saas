4749bb0ba011aadbef5164a1af1f26cc474b4add
Taufiq Muhammadi <waruboy@yahoo.com>
2017-02-15 08:16:02 +0700

    project model

---
+++ app/models/project.rb
@@ -0,0 +1,25 @@
+class Project < ActiveRecord::Base
+  belongs_to :tenant
+
+  validates :title, uniqueness: true
+  validates :tenant, presence: true
+  validate :free_plan_can_only_have_one_project
+
+  def self.by_plan_and_tenant(tenant_id)
+    tenant = Tenant.find(tenant_id)
+    if tenant.plan == 'premium'
+      tenant.projects
+    else
+      tenant.projects.order(:id).limit(1)
+    end
+  end
+
+
+  def free_plan_can_only_have_one_project
+    return unless (self.new_record? && (tenant.project.count > 0) && (tenant.plan == 'free'))
+
+    errors.add(:base, "Free plans cannot have more than one project")
+  end
+end
+
+
